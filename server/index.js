615f7f79d16c6da47ab8ec3153318df64d9f730d
rai983 <raika.0404gaaannn@gmail.com>
2024-10-06 16:35:59 +0900

    add hosting

--- server/index.js
+++ server/index.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("./config/dev");
+const config = require("./config");
 const FakeDb = require("./fake-db");
 const productRoutes = require("./routes/products");
 
-mongoose.connect(config.DB_URI, {}).then(() => {
-  const fakeDb = new FakeDb();
-  fakeDb.initDb();
-});
+const path = require("path");
+
+mongoose
+  .connect(config.DB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    if (process.env.NODE_ENV !== "production") {
+      const fakeDb = new FakeDb();
+      // fakeDb.initDb();
+    }
+  });
 
 const app = express();
 
 app.use("/api/v1/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const appPath = path.join(
+    __dirname,
+    "..",
+    "dist",
+    "angular-practice",
+    "browser"
+  );
+  app.use(express.static(appPath));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(appPath, "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || "3001";
 
